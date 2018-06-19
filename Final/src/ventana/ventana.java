832ba7c1d92849bea681cb1b5b92795fdf360d75
LN710Q <00004017@uca.edu.sv>
2018-06-19 08:22:52 -0600

    trabajando la ventana x2

--- Final/src/ventana/ventana.java
+++ Final/src/ventana/ventana.java
@@ -5,11 +5,14 @@
  */
 package ventana;
 
+import FiltroDAO.filtroDAO;
+import Modelo.alumno;
 import java.awt.Container;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  *
@@ -38,7 +41,7 @@ public class ventana extends JFrame {
         super("Inventario");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        //agregarLabels();
+        agregarLabels();
         //formulario();
         //llenarTabla();
         Container container = getContentPane();
@@ -49,6 +52,7 @@ public class ventana extends JFrame {
         container.add(carnet);
         container.add(nombre);
         container.add(apellidos);
+        container.add(edad);
         container.add(si);
         container.add(no);
         container.add(buscar);
@@ -61,13 +65,95 @@ public class ventana extends JFrame {
         //eventos();
 
     }
-   public static void main(String[] args) {
+
+    public final void agregarLabels() {
+        lblcarnet = new JLabel("Carnet");
+        lblNombres = new JLabel("Nombres");
+        lblApellidos = new JLabel("Apellidos");
+        lblUniversidad = new JLabel("Universidad");
+        lblEdad = new JLabel("Edad");
+        lblcarnet.setBounds(10, 10, ANCHOC, ALTOC);
+        lblNombres.setBounds(10, 60, WIDTH, HEIGHT);
+        lblApellidos.setBounds(60, 60, ANCHOC, ALTOC);
+        lblEdad.setBounds(10, 100, ANCHOC, ALTOC);
+        lblUniversidad.setBounds(10, 140, ANCHOC, ALTOC);
+    }
+
+    public final void formulario() {
+        carnet = new JTextField();
+        Universidad = new JComboBox();
+        nombre = new JTextField();
+        apellidos = new JTextField();
+        edad = new JTextField();
+
+        si = new JRadioButton("Si", true);
+        no = new JRadioButton("no");
+        resultados = new JTable();
+        buscar = new JButton("buscar");
+        insertar = new JButton("Insertar");
+        eliminar = new JButton("Eliminar");
+        actualizar = new JButton("Actualizar");
+        limpiar = new JButton("limpiar");
+        table = new JPanel();
+        Universidad.addItem("UCA");
+        Universidad.addItem("UDB");
+        Universidad.addItem("UFG");
+        Universidad.addItem("UGB");
+        estado = new ButtonGroup();
+        estado.add(si);
+        estado.add(no);
+        carnet.setBounds(140, 10, ANCHOC, ALTOC);
+        nombre.setBounds(140, 60, ANCHOC, ALTOC);
+        apellidos.setBounds(140, 100, ANCHOC, ALTOC);
+        si.setBounds(140, 140, 50, ALTOC);
+        no.setBounds(210, 140, 50, ALTOC);
+        buscar.setBounds(300, 10, ANCHOC, ALTOC);
+        insertar.setBounds(10, 210, ANCHOC, ALTOC);
+        actualizar.setBounds(150, 210, ANCHOC, ALTOC);
+        eliminar.setBounds(300, 210, ANCHOC, ALTOC);
+        limpiar.setBounds(450, 210, ANCHOC, ALTOC);
+        resultados = new JTable();
+        table.setBounds(10, 250, 500, 200);
+        table.add(new JScrollPane(resultados));
+
+    }
+       public void llenarTabla() {
+        tm = new DefaultTableModel() {
+            public Class<?> getColumnClass(int column) {
+                switch (column) {
+                    case 0:
+                        return String.class;
+                    case 1:
+                        return String.class;
+                    case 2:
+                        return String.class;
+                    default:
+                        return Boolean.class;
+                }
+            }
+        };
+        tm.addColumn("Codigo");
+        tm.addColumn("Marca");
+        tm.addColumn("Stock");
+        tm.addColumn("Stock en Sucursal");
+        filtroDAO fd = new filtroDAO();
+        ArrayList<alumno> alumnos = fd.readAll();
+        for (alumno i : alumnos) {
+            tm.addRow(new Object[]{i.getCodigo(), fi.getMarca(), fi.getStock()});
+        }
+        
+        resultados.setModel(tm);
+        
+    }
+
+
+    public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
                 new ventana().setVisible(true);
             }
-            
+
         });
     }
 }
